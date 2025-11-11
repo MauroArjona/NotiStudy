566cf562f9e07fa863b13612859a37f8c12c25b5
Zair02 <110054488+Zair02@users.noreply.github.com>
2025-11-11 18:31:57 -0300

    Color barra inferior

--- app/_layout.js
+++ app/_layout.js
@@ -3,12 +3,15 @@ import { Slot } from "expo-router";
 import { View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Header from "../components/Header";
+import * as NavigationBar from 'expo-navigation-bar';
 import { useEffect } from "react";
 import { initDB } from "../database/db";
 
 export default function Layout() {
   useEffect(() => {
     initDB(); // Se ejecuta una vez al iniciar la app
+    
+    NavigationBar.setButtonStyleAsync('light'); // botones blancos
   }, []);
 
   return (
